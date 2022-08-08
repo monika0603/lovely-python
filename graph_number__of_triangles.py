eb5f9d56b976231217134bcae2dbba1112d217ef
Monika Sharma <monika.pu@gmail.com>
2022-08-08 17:59:16 -0400

    Number of triangles in a graph

---
+++ graph_number__of_triangles.py
@@ -0,0 +1,28 @@
+
+import collections 
+def number_of_triangles(edges):
+
+    graph = build_graph(edges) 
+
+    count = 0 
+
+    for x, y in edges:
+        for node in graph:
+            if x in graph[node] and y in graph[node]:
+                count += 1 
+
+    return count//3
+
+def build_graph(edges):
+
+    graph = collections.defaultdict(list)
+
+    for x, y in edges:
+        graph[x].append(y)
+        graph[y].append(x) 
+
+    return graph 
+
+
+edges = [[0,1], [3,0], [0,2], [3,2], [1,2], [4,0], [3,4], [3,5], [4,5], [1,5], [1,3]]
+print(number_of_triangles(edges))
