eb3a4ad44fdd34c9c9ee980d6cb837404a63b6f9
Monika Sharma <monika.pu@gmail.com>
2022-08-24 17:55:28 -0400

    return a boolean indicating whether or not the value is contained in the tree

---
+++ tree_includes.py
@@ -0,0 +1,187 @@
+""" 
+Write a function, tree_includes, that takes in the root of a binary tree and a target value. 
+The function should return a boolean indicating whether or not the value is contained in the tree.
+"""
+class Node:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val 
+        self.left = left 
+        self.right = right 
+
+def tree_includes_recur(node, target):
+    if node is None:
+        return False 
+
+    if node.val == target:
+        return True 
+
+    return tree_includes_recur(node.left, target) or tree_includes_recur(node.right, target)
+
+def tree_includes(node, target):
+    if node is None:
+        return False 
+
+    stack = [node]
+    while stack:
+        current = stack.pop()
+
+        if current.val == target:
+            return True 
+
+        if current.left is not None:
+            stack.append(current.left)
+        if current.right is not None:
+            stack.append(current.right)
+    return False 
+
+# Driver code
+# Test case 01
+if __name__ == "__main__":
+
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    print(tree_includes_recur(a, "e")) # -> True
+
+    # Test case 02
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    print(tree_includes_recur(a, "a")) # -> True
+
+    # Test case 03
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    print(tree_includes_recur(a, "n")) # -> False
+
+    # Test case 04
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    print(tree_includes_recur(a, "f")) # -> True 
+
+    # Test case 05
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    print(tree_includes_recur(a, "p")) # -> False
+
+    # Test case 06
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    print(tree_includes_recur(a, "p")) # -> False
