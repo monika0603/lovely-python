0a77e8c01209141f974879d8a475288b1a1d5ff4
Monika Sharma <monika.pu@gmail.com>
2022-08-25 10:39:29 -0400

    return the right-most value in the bottom-most level of the tree.

---
+++ tree_bottom_right_value.py
@@ -0,0 +1,144 @@
+""" 
+Write a function, bottom_right_value, that takes in the root of a binary tree. The function should 
+return the right-most value in the bottom-most level of the tree.
+
+You may assume that the input tree is non-empty.
+"""
+
+from collections import deque 
+class Node:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val 
+        self.left = left 
+        self.right = right 
+
+def bottom_right_value(node):
+    if node is None:
+        return 0 
+
+    queue = deque([node])
+    result = []
+
+    while queue:
+        current = queue.popleft() 
+
+        result.append(current.val)
+
+        if current.left is not None:
+            queue.append(current.left)
+        if current.right is not None:
+            queue.append(current.right)
+
+    return result[-1]
+
+# Driver code 
+if __name__ == "__main__":
+    a = Node(3)
+    b = Node(11)
+    c = Node(10)
+    d = Node(4)
+    e = Node(-2)
+    f = Node(1)
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #       3
+    #    /    \
+    #   11     10
+    #  / \      \
+    # 4   -2     1
+
+    print(bottom_right_value(a)) # -> 1
+
+    # Test case 02
+    a = Node(-1)
+    b = Node(-6)
+    c = Node(-5)
+    d = Node(-3)
+    e = Node(-4)
+    f = Node(-13)
+    g = Node(-2)
+    h = Node(6)
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    e.right = h
+
+    #        -1
+    #      /   \
+    #    -6    -5
+    #   /  \     \
+    # -3   -4   -13
+    #     / \       
+    #    -2  6
+
+    print(bottom_right_value(a)) # -> 6
+
+    # Test case 03
+    a = Node(-1)
+    b = Node(-6)
+    c = Node(-5)
+    d = Node(-3)
+    e = Node(-4)
+    f = Node(-13)
+    g = Node(-2)
+    h = Node(6)
+    i = Node(7)
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    e.right = h
+    f.left = i
+
+    #        -1
+    #      /   \
+    #    -6    -5
+    #   /  \     \
+    # -3   -4   -13
+    #     / \    /   
+    #    -2  6  7 
+
+    print(bottom_right_value(a)) # -> 7
+
+    # Test case 04
+    a = Node('a')
+    b = Node('b')
+    c = Node('c')
+    d = Node('d')
+    e = Node('e')
+    f = Node('f')
+
+    a.left = b
+    a.right = c
+    b.right = d
+    d.left = e
+    e.right = f
+
+    #      a
+    #    /   \ 
+    #   b     c
+    #    \
+    #     d
+    #    /
+    #   e
+    #  /
+    # f
+            
+    print(bottom_right_value(a)) # -> 'f'
+
+    # Test case 05
+    a = Node(42)
+    #      42
+    print(bottom_right_value(a)) # -> 42
