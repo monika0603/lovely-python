649d4449cb60b29de2d85109666fcdafdf7f31f8
Monika Sharma <monika.pu@gmail.com>
2022-08-25 16:33:58 -0400

    return a boolean indicating whether or not the value is contained in the tree.

---
+++ trees/tree_includes.py
@@ -0,0 +1,136 @@
+
+class Node:
+    def __init__(self, val, left=None, right=None) -> None:
+        self.val = val 
+        self.left = left 
+        self.right = right 
+
+def tree_includes(root, target):
+    if root is None:
+        return False 
+
+    if root.val == target:
+        return True 
+
+    return tree_includes(root.left, target) or tree_includes(root.right, target)
+
+if __name__ == "__main__":
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    print(tree_includes(a, "e")) # -> True
+
+    # Test case 02
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    print(tree_includes(a, "a")) # -> True
+
+    # Test case 03
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+
+    print(tree_includes(a, "n")) # -> False
+
+    # Test case 04
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    print(tree_includes(a, "f")) # -> True
+
+    # Test case 05
+    a = Node("a")
+    b = Node("b")
+    c = Node("c")
+    d = Node("d")
+    e = Node("e")
+    f = Node("f")
+    g = Node("g")
+    h = Node("h")
+
+    a.left = b
+    a.right = c
+    b.left = d
+    b.right = e
+    c.right = f
+    e.left = g
+    f.right = h
+
+    #      a
+    #    /   \
+    #   b     c
+    #  / \     \
+    # d   e     f
+    #    /       \
+    #   g         h
+
+    print(tree_includes(a, "p")) # -> False
