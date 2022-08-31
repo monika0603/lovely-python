b69ffc6c9d6b26c448312062567e505ddcb27ce9
Monika Sharma <monika.pu@gmail.com>
2022-08-31 16:25:21 -0400

    Return the kth smallest element of the sorted matrix.

---
+++ k-smallest-matrix.py
@@ -0,0 +1,30 @@
+""" 
+Problem: Say you have an nxn matrix of elements that are sorted in ascending order both in the 
+column and rows of the matrix. Return the kth smallest element of the matrix. For example, 
+consider the matrix below:
+[1 4 7]
+[3 5 9]
+[6 8 11]
+"""
+
+import heapq 
+
+def smallest(grid, k):
+
+    minHeap = []
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            minHeap.append(grid[row][col])
+
+    heapq.heapify(minHeap)
+
+    return minHeap[k]
+
+if __name__ == "__main__":
+
+    grid = [ 
+        [1, 4, 7],
+        [3, 5, 9],
+        [6, 8, 11]
+    ]
+    print(smallest(grid, 5))
