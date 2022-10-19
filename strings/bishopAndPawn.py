70b02814e73248d97fa1e23f676c37dc8a507a24
Monika Sharma <monika.pu@gmail.com>
2022-10-19 16:15:51 -0400

    bishop /usr/local/bin/python3 /Users/monika.sharma1providence.org/Projects/lovely-python/strings/bishopAndPawn.py

---
+++ strings/bishopAndPawn.py
@@ -0,0 +1,21 @@
+""" 
+Given the positions of a white bishop and a black pawn on the standard chess 
+board, determine whether the bishop can capture the pawn in one move.
+
+The bishop has no restrictions in distance for each move, but is limited to 
+diagonal movement. Check out the example below to see how it can move:
+"""
+
+def bishopPawan(bishop, pawn):
+
+    return abs(int(bishop[1])-int(pawn[1])) == abs(ord(bishop[0]) - ord(pawn[0]))
+
+if __name__ == "__main__":
+    bishop = 'a1'
+    pawn = 'c3'
+    print(bishopPawan(bishop, pawn))
+
+    # Test case 01
+    bishop = 'h1'
+    pawn = 'h3'
+    print(bishopPawan(bishop, pawn))
